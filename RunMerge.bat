239439c01108a15fee6567f736ed7737e13393e7
prat1393 <40213116+prat1393@users.noreply.github.com>
2018-06-14 20:45:17 +0530

    Add files via upload

---
+++ RunMerge.bat
@@ -0,0 +1,5 @@
+@ECHO OFF
+REM Batch file to generate merge image for 1603_GRID_ON
+REM @author: Aamair Mirza
+ECHO ********** Starting merge for 1603_MYATT_GRID_ON ************
+ant merge-process-all
